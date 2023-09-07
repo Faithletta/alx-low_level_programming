c2777feadc1de7fd565d1c0f94ad67cec82984c4
Faithletta <faithmarl81@gmail.com>
2023-09-07 11:59:00 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 
 /**
  * main - prints "programming is like building a multilingual
- * puzzle, followed bu a newline. 
+ * puzzle, followed by a newline. 
  * Return: Always 0.
  */
+
+
 int main(void)
+
 {
     puts("\"programming is like building a multilingual puzzle");
     return (0);
