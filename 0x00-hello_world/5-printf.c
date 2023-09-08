66ddaf624b100da97c8e788a6c263150ad028f26
Faithletta <faithmarl81@gmail.com>
2023-09-07 18:28:05 -0700

    5-printf project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 /**
- * main - this is a print function
- *
- * Return: Always 0 
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
