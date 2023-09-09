769d887f894a7a98c495c1d10d6905f0dba90f46
Faithletta <faithmarl81@gmail.com>
2023-09-08 19:53:16 -0700

    All project Mofification and commit to the repo of variables, while loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabetic
- * Return: Always 0
+ * main - Prints the alphabet.
  *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
+
